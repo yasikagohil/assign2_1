c6a5752ff2f1c20c4d315a20b20a48e446eca9b9
yasikagohil <Yasika.gohil@gmail.com>
2021-10-11 14:18:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require('multer');
+var app = express();
+
+app.use(bodyParser.json());
+
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+var upload = multer({ storage : storage }).array('userPhoto',2);
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        //console.log(req.body);
+        //console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is successfully uploaded");
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
